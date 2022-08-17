2494eb3882f7d6f80e10f2ce4f0f48f2958efc66
Bnyro <yannick.fehler@web.de>
2022-08-17 08:49:36 +0200

    fix warning

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import i18n from "@intlify/vite-plugin-vue-i18n";
 import Unocss from "unocss/vite";
+import path from "path";
 
 // https://vitejs.dev/config/
-export default {
-  plugins: [vue(), Unocss()],
-};
+export default defineConfig({
+  plugins: [
+    vue(),
+    Unocss(),
+    i18n({
+      include: path.resolve(__dirname, "./src/locales/**"),
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
