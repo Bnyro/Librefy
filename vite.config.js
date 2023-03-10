59dabd24a0f108b049499eb4a684b1c6ba76a38b
Bnyro <bnyro@tutanota.com>
2023-03-10 16:48:16 +0100

    Fix vue i18n vite

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,21 @@
-import {
-    defineConfig
-} from "vite";
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import i18n from "@intlify/vite-plugin-vue-i18n";
+import i18n from "@intlify/unplugin-vue-i18n/vite";
 import Unocss from "unocss/vite";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        Unocss(),
-        i18n({
-            include: path.resolve(__dirname, "./src/locales/**"),
-        }),
-    ],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+  plugins: [
+    vue(),
+    Unocss(),
+    i18n({
+      include: path.resolve(__dirname, "./src/locales/**"),
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
 });
